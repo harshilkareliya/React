98aca247ed6cb7920ed2733d8fd93ec02e0083f3
harshilkareliya <kareliyaharshil2111@gmail.com>
2024-04-12 10:47:15 +0530

    task complete

---
+++ 04seteffect/src/Test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState, useEffect} from 'react'
+
+
+function Test() {
+  const [test, setTest] = useState(null)
+  useEffect(()=>{
+    const fetching = async () =>{
+      let responce = await fetch('https://restcountries.com/v3.1/all')
+      // let responce = await fetch('https://fakestoreapi.com/products')
+      let jsonData = await responce.json()
+      console.log(jsonData)
+    }
+    fetching()
+  },[])
+  return (
+    <div>
+  
+    </div>
+  )
+}
+
+export default Test
