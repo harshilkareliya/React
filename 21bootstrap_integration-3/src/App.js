72a829c10a4ad72e7249dd9c0cdaba27549874be
harshilkareliya <kareliyaharshil2111@gmail.com>
2024-05-07 10:24:10 +0530

    task complete

--- 21bootstrap_integration-3/src/App.js
+++ 21bootstrap_integration-3/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './Components/About';
+import Clients from './Components/Clients';
+import Contact from './Components/Contact';
+import Count from './Components/Count';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import Portofolio from './Components/Portofolio';
+import Service from './Components/Service';
+import Team from './Components/Team';
+import Testimonials from './Components/Testimonials';
+import Why_us from './Components/Why_us';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Hero/>
+      <main id='main'>
+        <Clients/>
+        <About/>
+        <Count/>
+        <Why_us/>
+        <Service/>
+        <Portofolio/>
+        <Testimonials/>
+        <Team/>
+        <Contact/>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
