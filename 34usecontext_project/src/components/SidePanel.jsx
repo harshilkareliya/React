3cff876d307e52119e0414e5fe9ecdf071b9816e
harshilkareliya <kareliyaharshil2111@gmail.com>
2024-05-30 18:23:50 +0530

    task complete

--- 34usecontext_project/src/components/SidePanel.jsx
+++ 34usecontext_project/src/components/SidePanel.jsx
@@ -1,26 +1,35 @@
-import React,{useState, useMemo} from 'react'
+import React, { useState, useMemo } from 'react'
 import './SidePanel.css'
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
+import { Link } from 'react-router-dom'
 
 function SidePanel() {
 
-    const[show, setshow] = useState(false)
+    const [show, setshow] = useState(false)
 
-    const position = useMemo(()=>{
-        return{
-            width : show ? '18%' : '0'
+    const position = useMemo(() => {
+        return {
+            width: show ? '18%' : '0',
+            display: show ? 'block' : 'none',
         }
     })
 
     return (
         <div style={{ display: 'flex' }}>
             <div className='showside'>
-                <span className='icon' onClick={()=>setshow(!show)}>
+                <span className='icon' onClick={() => setshow(!show)}>
                     <HiOutlineMenuAlt1 />
                 </span>
             </div>
-            <div className="toggleSide" style={{width : position.width}}>
+            <div className="toggleSide" style={{ width: position.width }}>
+                <br />
+                <div style={{ display: position.display }} id='box' >
+                    <h1>HK's</h1><br />
 
+                    <h2><Link to='/' className='h2'>Home</Link></h2>
+                    <h2><Link to='/' className='h2'>Portfolio</Link></h2>
+                    <h2><Link to='/about' className='h2'>About</Link></h2>
+                </div>
             </div>
         </div>
     )
